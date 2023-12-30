9c37e7dfa233c3aec34d9c6cc8f8e7de2250f1d3
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2023-12-30 11:40:25 +0700

    signal chart

--- SportFlux/src/components/SignalChart.tsx
+++ SportFlux/src/components/SignalChart.tsx
@@ -1,54 +1,60 @@
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
-const SignalChart: React.FC = () => {
+const SignalChart = () => {
   const chartRef = useRef<HTMLCanvasElement | null>(null);
+  const maxDataPoints = 10;
 
   useEffect(() => {
     let chart: Chart<"line"> | null = null;
 
-    if (chartRef.current) {
-      const ctx = chartRef.current.getContext('2d');
-      if (ctx) {
-        chart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: [],
-            datasets: [
-              {
-                label: 'Signal Strength',
-                data: [],
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 2,
-                fill: false,
-              },
-            ],
-          },
-          options: {
-            animation: {
-              duration: 0, // Disable animation for real-time updates
+    const initializeChart = () => {
+      if (chartRef.current) {
+        const ctx = chartRef.current.getContext('2d');
+        
+        if (ctx) {
+          chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: [],
+              datasets: [
+                {
+                  label: 'Signal Strength',
+                  data: [],
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 2,
+                  fill: false,
+                },
+              ],
             },
-            scales: {
-              x: {
-                type: 'linear',
-                position: 'bottom',
+            options: {
+              animation: {
+                duration: 0, // Disable animation for real-time updates
               },
-              y: {
-                beginAtZero: true,
+              scales: {
+                x: {
+                  type: 'linear',
+                  position: 'bottom',
+                },
+                y: {
+                  beginAtZero: true,
+                },
               },
             },
-          },
-        });
+          });
 
-        // Dummy data for testing
-        const dummyData = [10, 20, 30, 40, 50];
-        let counter = 0;
+          const dummyData = [10, 20, 30, 40, 50];
+          let counter = 0;
 
-        setInterval(() => {
-          if (chart) {
-            const newData = Math.floor(Math.random() * 100);
+          setInterval(() => {
+            if (chart) {
+              const newData = Math.floor(Math.random() * 100);
 
-            if (chart.data.labels && chart.data.datasets[0].data) {
+              if (chart.data.labels.length >= maxDataPoints) {
+                chart.data.labels.shift();
+                chart.data.datasets[0].data.shift();
+              }
+
               chart.data.labels.push(counter);
               chart.data.datasets[0].data.push(newData);
 
@@ -56,10 +62,12 @@ const SignalChart: React.FC = () => {
 
               chart.update();
             }
-          }
-        }, 100); // Update more frequently (adjust as needed for smoothness)
+          }, 100); // Update more frequently (adjust as needed for smoothness)
+        }
       }
-    }
+    };
+
+    initializeChart();
 
     return () => {
       if (chart) {
