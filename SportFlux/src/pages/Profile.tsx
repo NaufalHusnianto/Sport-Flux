de9edbad6447d4391ba232a6c802572be7aee174
Naufal Husnianto <111817684+NaufalHusnianto@users.noreply.github.com>
2024-01-09 11:38:45 +0700

    update activity

--- SportFlux/src/pages/Profile.tsx
+++ SportFlux/src/pages/Profile.tsx
@@ -49,76 +49,7 @@ const UserProfile: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
-        <IonCard color={'primary'} style={{ borderRadius: '30px', background: 'rgba(0, 0, 0, 0.3)' }}>
-          <IonCardTitle className='text-center mt-3'>
-            <p style={{ margin: 'auto', width: '90%', background: '#122D3B' }} className='rounded-pill text-white fs-7 p-1'>My Activity</p>
-          </IonCardTitle>
-
-          <IonCard color={'primary'} style={{background: 'rgba(0,0,0,0)', boxShadow: 'none'}}>
-            <IonGrid>
-              <IonRow style={{ display: 'flex', height: '100%', borderRadius: '30px', overflow: 'hidden', background: '#122D3B' }}>
-                <IonCol size='5' className='text-center text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p style={{background: 'green'}} className='rounded-pill text-white'>EMG Record</p>
-                  <IonImg src='pulse.png' style={{width: '50%'}} className='m-2'/>
-                </IonCol>
-                <IonCol size='7' className='text-left text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p className='m-0'>Device : 5</p>
-                  <p className='m-0'>Channel : 10</p>
-                  <p className='m-0'>Status : Diagnosed</p>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCard>
-
-          <IonCard color={'primary'} style={{background: 'rgba(0,0,0,0)', boxShadow: 'none'}}>
-            <IonGrid>
-              <IonRow style={{ display: 'flex', height: '100%', borderRadius: '30px', overflow: 'hidden', background: '#122D3B' }}>
-                <IonCol size='5' className='text-center text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p style={{background: 'green'}} className='rounded-pill text-white'>EMG Record</p>
-                  <IonImg src='pulse.png' style={{width: '50%'}} className='m-2'/>
-                </IonCol>
-                <IonCol size='7' className='text-left text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p className='m-0'>Device : 5</p>
-                  <p className='m-0'>Channel : 10</p>
-                  <p className='m-0'>Status : Diagnosed</p>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCard>
-
-          <IonCard color={'primary'} style={{background: 'rgba(0,0,0,0)', boxShadow: 'none'}}>
-            <IonGrid>
-              <IonRow style={{ display: 'flex', height: '100%', borderRadius: '30px', overflow: 'hidden', background: '#122D3B' }}>
-                <IonCol size='5' className='text-center text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p style={{background: 'green'}} className='rounded-pill text-white'>EMG Record</p>
-                  <IonImg src='pulse.png' style={{width: '50%'}} className='m-2'/>
-                </IonCol>
-                <IonCol size='7' className='text-left text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p className='m-0'>Device : 5</p>
-                  <p className='m-0'>Channel : 10</p>
-                  <p className='m-0'>Status : Diagnosed</p>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCard>
-
-          <IonCard color={'primary'} style={{background: 'rgba(0,0,0,0)', boxShadow: 'none'}}>
-            <IonGrid>
-              <IonRow style={{ display: 'flex', height: '100%', borderRadius: '30px', overflow: 'hidden', background: '#122D3B' }}>
-                <IonCol size='5' className='text-center text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p style={{background: 'green'}} className='rounded-pill text-white'>EMG Record</p>
-                  <IonImg src='pulse.png' style={{width: '50%'}} className='m-2'/>
-                </IonCol>
-                <IonCol size='7' className='text-left text-white p-3 d-flex flex-column justify-content-center' style={{ flex: '1' }}>
-                  <p className='m-0'>Device : 5</p>
-                  <p className='m-0'>Channel : 10</p>
-                  <p className='m-0'>Status : Diagnosed</p>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCard>
-
-        </IonCard>
+        
       </IonContent>
     </IonPage>
   );
