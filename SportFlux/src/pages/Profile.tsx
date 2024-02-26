ccffdab8618e529ad8148f544338cfcbdf44fe98
Heparia <112355993+Heparia@users.noreply.github.com>
2024-02-26 09:38:44 +0700

    profil
    perbaikan

--- SportFlux/src/pages/Profile.tsx
+++ SportFlux/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonCardTitle, IonIcon, IonHeader, IonPage, IonGrid, IonRow, IonCol, IonImg, IonCard, IonButton, IonToolbar, IonCardHeader, IonCardContent, IonList, IonItem } from '@ionic/react';
-import { settings } from 'ionicons/icons';
+import { IonContent, IonCardTitle, IonIcon, IonHeader, IonPage, IonGrid, IonRow, IonCol, IonImg, IonCard, IonButton, IonToolbar, IonCardHeader, IonCardContent, IonList, IonItem, IonText, IonAccordion, IonAccordionGroup, IonLabel } from '@ionic/react';
+import { settings, chevronForwardOutline, settingsOutline, carOutline, chatbubbleEllipsesOutline, helpCircleOutline, giftOutline, ribbonOutline, heartOutline, timeOutline, cubeOutline, walletOutline, cartOutline, clipboardOutline } from 'ionicons/icons';
 import { logoutUser } from '../firebaseConfig';
 import { useHistory } from "react-router-dom";
 
@@ -17,164 +17,150 @@ const UserProfile: React.FC = () => {
   return (
     <IonPage>
       <IonContent color={'tertiary'}>
-        <IonCard className='ion-no-border m-0' style={{ borderBottomLeftRadius: '50px', borderBottomRightRadius: '50px', background: 'rgba(0, 0, 0, 0.3)', boxShadow: '2px 2px 5px #0b0b0b', zIndex: 9999}}>
-            <IonGrid>
-              <IonRow>
-                <IonCol className='d-flex justify-content-center align-item-center'>
-                  <IonImg src='LOGO.png' style={{ width: '30%', height: 'auto' }} />
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol size='4' className='w-100 h-100'>
-                  <IonImg src='/profile.png' style={{ background: 'green', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)', borderRadius: '20px' }}/>
-                </IonCol>
-                <IonCol size='8'>
-                  <IonCard className='w-100 p-2 pb-3' color={'secondary'} style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)', margin: 0}}>
-                        <IonRow>
-                            <IonCol size='auto'>
-                                <p className='mt-0 mb-1'>SportFlux User 1</p>
-                            </IonCol>
-                            <IonCol>
-                                <IonImg src='/exclusive_icon.png' style={{width: '20px'}}/>
-                            </IonCol>
+        <IonCard className='ion-no-border m-0' style={{ background: 'transparent', boxShadow: 'none'}}>
+            <IonGrid className='p-0 ion-margin-top ion-no-border' style={{ boxShadow: 'white' }}>
+                <IonRow style={{ alignItems:'top' }}>
+                    <IonCol className='p-0' size='3'>
+                        <IonRow className='p-1' style={{ display: 'flex', justifyContent: 'space-around', alignItems:'center', backgroundColor:'white', borderRadius:'0 20px 20px 0', color:'var(--ion-color-tertiary)' }}>
+                            <IonText color='tertiary'>
+                                <p className='m-0' style={{ fontWeight:'bold'}}>Start</p>
+                            </IonText>
+                            <IonIcon icon={chevronForwardOutline} color='tertiary'></IonIcon>
                         </IonRow>
-                        <p style={{ background: '#008100', padding: '3px', borderRadius: '10px', width: '80%', marginBottom: '5px'}}>80% healthy</p>
-                        <p style={{ background: '#3E7290', padding: '3px', borderRadius: '10px', width: '100%', margin: '0'}}>100 EMG Record</p>
-
-                  </IonCard>
-                </IonCol>
-              </IonRow>
-              <IonRow className='mt-0'>
-                <IonCol className='text-center'>
-                  <IonButton className='mt-0 mb-4 w-100' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>Change Profile</IonButton>
-                </IonCol>
-              </IonRow>
+                    </IonCol>
+                    <IonCol></IonCol>
+                    <IonCol size='auto' style={{ marginRight:'20px'}}>
+                        <IonIcon icon={cartOutline} color='light' style={{ fontSize:'35px'}}></IonIcon>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <img src='./profile.png' style={{ backgroundColor:'white', borderRadius:'100%', width:'100px', margin:'auto'}}></img>
+                </IonRow>
+                <IonRow>
+                    <h1 style={{ margin:'10px auto 0px', fontSize:'30px', color:'white' }}>Clara Alverina</h1>
+                </IonRow>
+                <IonRow>
+                    <IonText className='m-auto' color='tertiary'>
+                        <h6 style={{ backgroundColor:'white', padding:'4px 20px', borderRadius:'20px' }}>Classic Member</h6>
+                    </IonText>
+                </IonRow>
+                <IonRow>
+                   <p style={{ margin:'10px auto', color:'white' }}>1.232 Points</p>
+                </IonRow>
+                <IonRow className='mt-2'>
+                    <IonCol>
+                        <IonText color="tertiary">
+                            <p style={{ padding:'7px 10px', backgroundColor:'white', textAlign:'center', margin:'0px 10px 0px 35px', borderRadius:'15px', fontSize:'13px' }}>Follower: <strong>0</strong></p>
+                        </IonText>
+                    </IonCol>
+                    <IonCol>
+                        <IonText color="tertiary">
+                            <p style={{ padding:'7px 10px', backgroundColor:'white', textAlign:'center', margin:'0px 10px 0px 35px', borderRadius:'15px', fontSize:'13px' }}>Following: <strong>234</strong></p>
+                        </IonText>
+                    </IonCol>
+                </IonRow>
             </IonGrid>
         </IonCard>
-        <IonCard className='ion-no-border mt-4 m-0' style={{ borderRadius: '50px 50px 0 0', background: 'rgba(0, 0, 0, 0.3)', boxShadow: '2px 2px 5px #0b0b0b', zIndex: 9999}}>
-            <IonCardHeader className='p-3 m-4' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>
-               <IonCardTitle className='ion-text-center' style={{ fontSize: '20px' }}>My Activity</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-                <IonList style={{background: 'transparent'}}>
-
-                    <IonItem className='m-2 mt-0' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>
-                        <IonGrid fixed={true}>
-
-                                    <IonRow className='mt-2'>
-                                        <IonCol size='6' className='ion-text-center'>
-                                            <p style={{ background: '#008100', fontSize:'16px', width: 'fit-content', padding: '10px', borderRadius: '15px', margin: 'auto', fontWeight: 'bold' }}>EMG Record</p>
-                                        </IonCol>
-                                        <IonCol></IonCol>
-                                        <IonCol size='auto'>
-                                            <IonImg src='/exclusive_icon.png' style={{width: '50px'}}/>
-                                        </IonCol>
-                                    </IonRow>
-                                    <IonRow className='mb-2'>
-                                        <IonCol size='3' className='m-0'>
-                                            <IonImg src='/pulse.png' className='m-auto' style={{ border: '2px solid #fff', width: '100%', borderRadius: '10px'}}/>
-                                        </IonCol>
-                                        <IonCol style={{ fontSize: '13px' }}>
-                                            <IonRow>
-                                                <IonCol size='4'>Devices</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='5'>5</IonCol>
-                                            </IonRow>
-                                            <IonRow>
-                                                <IonCol size='4'>Channel</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='5'>10</IonCol>
-                                            </IonRow>
-                                            <IonRow>
-                                                <IonCol size='4'>Status</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='6'>Diagnosed</IonCol>
-                                            </IonRow>
-                                        </IonCol>
-                                    </IonRow>
-                        </IonGrid>
+        <IonCard className='ion-no-border mt-4 m-0' color='light' style={{ borderRadius: '40px 40px 0 0' }}>
+            <IonGrid className='m-3'>
+                <IonRow style={{ alignItems:'center', justifyContent:'center', borderBottom:'1px solid black'}}>
+                    <IonCol size='auto'>
+                        <IonText>
+                            <IonIcon icon={clipboardOutline} style={{ fontSize:'25px' }}></IonIcon>
+                        </IonText>
+                    </IonCol>
+                    <IonCol>
+                        <IonText className='m-0'>
+                            <p style={{ marginBottom: '0px' }}><strong>My Purchases</strong></p>
+                        </IonText>
+                    </IonCol>
+                    <IonCol className='m-0'>
+                        <IonText style={{ fontSize:'12px', textAlign:'right', fontWeight:'light' }}>
+                            <p style={{ marginBottom: '0px' }}>View Purchase History</p>
+                        </IonText>
+                    </IonCol>
+                </IonRow>
+                <IonRow className='mt-3'>
+                    <IonCol style={{ textAlign:'center' }}>
+                        <IonIcon icon={walletOutline} style={{ fontSize:'40px', textAlign:'center' }}></IonIcon>
+                        <p>To Pay</p>
+                    </IonCol>
+                    <IonCol style={{ textAlign:'center' }}>
+                        <IonIcon icon={carOutline} style={{ fontSize:'40px', textAlign:'center' }}></IonIcon>
+                        <p>To Shop</p>
+                    </IonCol>
+                    <IonCol style={{ textAlign:'center' }}>
+                        <IonIcon icon={cubeOutline} style={{ fontSize:'40px', textAlign:'center' }}></IonIcon>
+                        <p>To Receive</p>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+            <IonAccordionGroup>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={heartOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Liked</strong></IonLabel>
                     </IonItem>
-
-                    <IonItem className='m-2 mt-4' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>
-                        <IonGrid fixed={true}>
-
-                                    <IonRow className='mt-2'>
-                                        <IonCol size='6' className='ion-text-center'>
-                                            <p style={{ background: '#008100', fontSize:'16px', width: 'fit-content', padding: '10px', borderRadius: '15px', margin: 'auto', fontWeight: 'bold' }}>EMG Record</p>
-                                        </IonCol>
-                                        <IonCol></IonCol>
-                                        <IonCol size='auto'>
-                                            <IonImg src='/premium_icon.png' style={{width: '50px'}}/>
-                                        </IonCol>
-                                    </IonRow>
-                                    <IonRow className='mb-2'>
-                                        <IonCol size='3' className='m-0'>
-                                            <IonImg src='/pulse.png' className='m-auto' style={{ border: '2px solid #fff', width: '100%', borderRadius: '10px'}}/>
-                                        </IonCol>
-                                        <IonCol style={{ fontSize: '13px' }}>
-                                            <IonRow>
-                                                <IonCol size='4'>Devices</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='5'>3</IonCol>
-                                            </IonRow>
-                                            <IonRow>
-                                                <IonCol size='4'>Channel</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='5'>6</IonCol>
-                                            </IonRow>
-                                            <IonRow>
-                                                <IonCol size='4'>Status</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='6'>Recorded</IonCol>
-                                            </IonRow>
-                                        </IonCol>
-                                    </IonRow>
-                        </IonGrid>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={timeOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Recently Viewed</strong></IonLabel>
                     </IonItem>
-
-                    <IonItem className='m-2 mt-4' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>
-                        <IonGrid fixed={true}>
-
-                                    <IonRow className='mt-2'>
-                                        <IonCol size='6' className='ion-text-center'>
-                                            <p style={{ background: '#008100', fontSize:'16px', width: 'fit-content', padding: '10px', borderRadius: '15px', margin: 'auto', fontWeight: 'bold' }}>EMG Record</p>
-                                        </IonCol>
-                                        <IonCol></IonCol>
-                                        <IonCol size='auto'>
-                                            <IonImg src='/standart_icon.png' style={{width: '50px'}}/>
-                                        </IonCol>
-                                    </IonRow>
-                                    <IonRow className='mb-2'>
-                                        <IonCol size='3' className='m-0'>
-                                            <IonImg src='/pulse.png' className='m-auto' style={{ border: '2px solid #fff', width: '100%', borderRadius: '10px'}}/>
-                                        </IonCol>
-                                        <IonCol style={{ fontSize: '13px' }}>
-                                            <IonRow>
-                                                <IonCol size='4'>Devices</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='5'>10</IonCol>
-                                            </IonRow>
-                                            <IonRow>
-                                                <IonCol size='4'>Channel</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='5'>20</IonCol>
-                                            </IonRow>
-                                            <IonRow>
-                                                <IonCol size='4'>Status</IonCol>
-                                                <IonCol size='1'>:</IonCol>
-                                                <IonCol size='6'>Followed</IonCol>
-                                            </IonRow>
-                                        </IonCol>
-                                    </IonRow>
-                        </IonGrid>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={ribbonOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Loyalty</strong></IonLabel>
                     </IonItem>
-
-                </IonList>
-            </IonCardContent>
-        </IonCard>
-
-        <IonCard className='ion-no-border mt-4 m-0' style={{ borderRadius: '50px 50px 0 0', background: 'rgba(0, 0, 0, 0.3)', boxShadow: '2px 2px 5px #0b0b0b', zIndex: 9999}}>
-            <IonButton onClick={handleLogout} className='mt-0 mb-4 w-100' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>Logout</IonButton>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={giftOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Gift Voucher</strong></IonLabel>
+                    </IonItem>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={settingsOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Account Setting</strong></IonLabel>
+                    </IonItem>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={helpCircleOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Help Centre</strong></IonLabel>
+                    </IonItem>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="first">
+                    <IonItem slot="header" color="light">
+                        <IonIcon icon={chatbubbleEllipsesOutline} className='pe-2'></IonIcon>
+                      <IonLabel><strong>Chat with Sportflux</strong></IonLabel>
+                    </IonItem>
+                    <div className="ion-padding" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+            </IonAccordionGroup>
+            <IonButton onClick={handleLogout} className='mt-4 mb-4 w-100' style={{borderRadius: '20px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.8)'}} color={'secondary'}>Logout</IonButton>
         </IonCard>
       </IonContent>
     </IonPage>
